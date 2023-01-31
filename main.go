6fa52c3ddccfe594ad2572bb8c47dad1f9918612
zhuang111 <45065175+zhuang111@users.noreply.github.com>
2023-01-31 15:00:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ var (
 )
 
 func main() {
-	flag.StringVar(&listenAddr, "listen-addr", ":5000", "server listen address")
+	flag.StringVar(&listenAddr, "listen-addr", ":8080", "server listen address")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
